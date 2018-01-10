0edfb76d97ae4dedf303b1199efefb62847e7e4c
Jennifer Dobak <jdobak1@gmail.com>
2018-01-10 23:04:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator (array)
   array.collect {|name| badge_maker(name)}
 end
 
-def assign_rooms (arrayofnames)
+def assign_rooms (array)
   return "Hello #{name}! You'll be assigned to room #{room}!"
 end
 
