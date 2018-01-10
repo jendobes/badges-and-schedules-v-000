a03014d7d8da7b57deb326aad48a43b1e539ce89
Jennifer Dobak <jdobak1@gmail.com>
2018-01-10 22:59:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (arrayofnames)
+  return
+end
+
+def assign_rooms (arrayofnames)
+  return
+end
+
+def printer
+  puts
+end
